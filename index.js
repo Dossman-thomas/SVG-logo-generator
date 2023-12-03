f871ee9fb3ac9ec0f77baf735ac87a91a36b009d
Tom Dossman <tdossman.development@gmail.com>
2023-12-03 16:57:28 -0500

    inquirer questions and generateSVG.js added

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+// Import packages needed for this application:
+
+// Import Inquirer
+const inquirer = require('inquirer');
+// Import File System(fs)
+const fs = require('fs');
+// Import generateSVG.js
+const generateSVG = require('./lib/generateSVG');
+
+// Questions:
+const questions = [
+
+// Enter text for the logo (Must not be more than 3 characters long).
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter text for the logo (Must not be more than 3 characters long).'
+  },
+
+// Enter a text color
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter a text color.'
+  },
+
+// Select a shape for the logo
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Select a shape for your logo.',
+    choices: ['triangle', 'circle', 'square']
+  },
+
+// Enter a shape color
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter a shape color.'
+  },
+];
+
+
+// Function to initialize app
+function init() {
+
+  inquirer
+
+// Pass questions in here
+  .prompt(questions)
+  .then((data) => {
+    // Use user feedback data to generate SVG file
+
+    const svgContent = generateSVG(data);
+
+    console.log(svgContent);
+
+    // writes svg logo using generateShapes module and alerts any errors.  If no errors, console log success message.
+    // fs.writeFile('logo.svg', svgContent, (err) =>
+    // err ? console.log(err) : console.log('Successfully generated logo.svg file!')
+    // );
+
+  })
+
+}
+
+
+// Function call to initialize app
+init();
+
