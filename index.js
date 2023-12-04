795ce5391888b09b37ef6bac802e6db9a10606c8
Tom Dossman <tdossman.development@gmail.com>
2023-12-04 17:52:31 -0500

    file generation functionality complete

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 // Import Inquirer
 const inquirer = require('inquirer');
-// Import File System(fs)
-const fs = require('fs');
-// Import generateSVG.js
-const generateSVG = require('./lib/generateSVG');
 
-// Questions:
+// Import File System(fs)
+const fs = require('fs').promises; // Using fs.promises to make file operations promise-based
+
+// // Import shapes modules
+const {Triangle, Circle, Square} = require('./lib/shapes');
+
+//------------------------------------------------------
+
+// Inquirer questions saved in an array of objects:
 const questions = [
 
 // Enter text for the logo (Must not be more than 3 characters long).
@@ -40,31 +44,90 @@ const questions = [
   },
 ];
 
+//------------------------------------------------------
 
-// Function to initialize app
-function init() {
+// Svg class w/constructor w/methods for setting text and shape elements and returning a working svg file
+class Svg {
 
-  inquirer
+  constructor(){
+    this.textEl = ''; // initializes text element
+    this.shapeEl = ''; // initializes shape element
+  }
 
-// Pass questions in here
-  .prompt(questions)
-  .then((data) => {
-    // Use user feedback data to generate SVG file
+  render(){
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`
+  }
 
-    const svgContent = generateSVG(data);
+  setText(text, textColor){
+    this.textEl = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
+  }
 
-    console.log(svgContent);
-
-    // writes svg logo using generateShapes module and alerts any errors.  If no errors, console log success message.
-    // fs.writeFile('logo.svg', svgContent, (err) =>
-    // err ? console.log(err) : console.log('Successfully generated logo.svg file!')
-    // );
-
-  })
+  setShape(shape){
+    this.shapeEl = shape.render();
+  }
 
 }
 
+//------------------------------------------------------
+
+// Async function to initialize app
+async function init() {
+  try {
+
+    // pass questions here
+    const data = await inquirer.prompt(questions);
+
+    // ensuring user only enters up to 3 characters of text
+    let text = '';
+
+    if (data.text.length > 0 && data.text.length < 4){
+
+      text = data.text; // valid character length
+
+    } else { // invalid character length
+
+      console.log('Invalid character length. Text for logo cannot be any more than 3 characters long.'); // error message
+      
+      return; // end function
+    }
+
+    let shapeElement;
+
+    // Create the appropriate shape element based on user selection
+    switch (data.shape.toLowerCase()) {
+      case 'triangle':
+        shapeElement = new Triangle();
+        break;
+      case 'circle':
+        shapeElement = new Circle();
+        break;
+      case 'square':
+        shapeElement = new Square();
+        break;
+    }
+    
+    shapeElement.setColor(data.shapeColor); // sets shapeColor based on user input
+
+    console.log(shapeElement);
+
+    const svgFile = new Svg ();
+
+    svgFile.setText(text, data.textColor);
+    svgFile.setShape(shapeElement);
+
+    const svgContent = svgFile.render();
+
+    console.log(svgContent);
+    
+
+    // Writes SVG file using fs.promises.writeFile
+    await fs.writeFile('logo.svg', svgContent);
+
+    console.log('Successfully generated logo.svg file!');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
 // Function call to initialize app
 init();
-
